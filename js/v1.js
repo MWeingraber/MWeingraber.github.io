6daf0c64be2d0a7a425888d8dedd3e1d41bb8d6a
Moritz Weingraber <weingraber.m03@htlwienwest.at>
2020-11-19 09:28:21 +0100

    etwas

--- js/v1.js
+++ js/v1.js
@@ -1,3 +1,16 @@
 //console.log("test")
 let c = document.querySelectorAll(".card")
 console.log(c)
+
+function change(){
+    this.classList.add("flipped")
+    setTimeout(() => {  this.classList.remove("flipped");}, 500);
+}
+
+console.log("Test")
+let c = document.querySelectorAll('.card')
+console.log(c)
+
+for (const ttt of c) {
+    ttt.addEventListener('click', change)
+}
