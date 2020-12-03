794b60437ea8523132b39a5be153ccaa0e37f08d
Moritz Weingraber <weingraber.m03@htlwienwest.at>
2020-12-03 08:03:21 +0100

    V1

--- js/v1.js
+++ js/v1.js
@@ -1,22 +1,100 @@
-  
-const numberCards = 16
-let openedCards = []
+function Game(){
+    //Variablen
+    var VerfügbareIDs = ["a", "b", "c" , "d", "e", "f", "g", "h"];
+    var genommeneIDs1 = [];
+    var genommeneIDs2 = [];
 
-let p = document.querySelector('.deck')
-for (let index = 0; index < numberCards; index++) {
-        let c = document.createElement('div')
-        c.innerHTML = "Hallo "+ (index + 1)
-        c.type = index % 2 + 1
-        c.className="card"
-        c.addEventListener('click',flipp)
-        p.appendChild(c)
+    var umgedrehteKarten = [];
+    var ZeitSek = 0;
+    var Score = 0;
+
+    function Hochdrehen(event){
+        if(umgedrehteKarten.length != 2 && event.target.className != "card flipped"){
+            event.target.classList.toggle("flipped"); 
+            umgedrehteKarten.unshift(event.target);
+
+            Control();
+        }
+    }
+
+
+    function Control(){
+        if(umgedrehteKarten.length.valueOf() == 2){
+            if(umgedrehteKarten[0].id == umgedrehteKarten[1].id){
+                
+                Score += 1
+                document.querySelector('#Überschrift').textContent = `Score: ${Score}`;
+
+                while(umgedrehteKarten.length > 0){
+                    umgedrehteKarten.pop();
+                }
+            }
+            else{
+                setTimeout(function(){
+                    umgedrehteKarten.forEach(element => {
+                        element.classList.toggle("flipped");
+
+                    });
+                    
+                    while(umgedrehteKarten.length > 0){
+                        umgedrehteKarten.pop();
+                    }
+
+                }, 500);
+            }
+        }
+
+        if(Score == 8){
+            setTimeout(() => {
+                window.alert(`Du hast es geschafft (Innerhalb von ${ZeitSek} Sekunden)!`);
+                location.reload();
+            }, 1000);
+        }
+
+    }
+
+    function CardErstellen(){
+        var Container = document.querySelector('#card-deck');
+
+        for (let index = 1; index < 17; index++) {
+            var nCard = document.createElement("div");
+            nCard.classList.add("card");
+            nCard.setAttribute("id", VerfügbareIDs[GetRightID()-1]);
+            Container.appendChild(nCard);
+        }
+    }
+
+
+    function GetRightID(){
+        for (let index = 0; index < 300; index++) {
+            var idIndexGef = Math.round((Math.random() * (8 - 1) + 1));
+
+            if(genommeneIDs1.indexOf(VerfügbareIDs[idIndexGef]) == -1 && idIndexGef != 0){
+                genommeneIDs1.push(VerfügbareIDs[idIndexGef]);
+                return idIndexGef;
+            }
+            else if(genommeneIDs2.indexOf(VerfügbareIDs[idIndexGef]) == -1 && idIndexGef != 0){
+                genommeneIDs2.push(VerfügbareIDs[idIndexGef]);
+                return idIndexGef;
+            }
+            else{
+                continue;
+            }
+        }
+    }
+
+    function CardEventsErstellen(){
+        let CardList = document.querySelectorAll('.card');
+        CardList.forEach(element => {
+            element.addEventListener('click', Hochdrehen);
+        });
+    }
+
+    CardErstellen();
+    CardEventsErstellen();
+    var Timer = setInterval(function(){
+        ZeitSek += 1;            
+    }, 1000);
 }
 
-function flipp(event){
-    if(!this.classList.contains('found'))
-        openCard(this)
-    //this.classList.toggle('flipped')
-}
-function openCard(c){
-    alert(c.innerHTML)
-}
+Game();
